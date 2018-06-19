d9222ea2f05904949005d3a53c9a9fa69e2ca320
Sonu Sharma <kumarsonu676@gmail.com>
2018-06-19 20:40:02 +0530

    Update dbConfig.js

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -2,10 +2,10 @@
 var dbAllConfig = {
 
     config: {
-        server: 'SONU',
-        database: 'TutorialTest',
-        user: 'sa',
-        password: 'pat@111'
+        server: 'SERVER_NAME',
+        database: 'YOUR_DB_NAME',
+        user: 'YOUR_SQL_SERVER_USER_NAME',
+        password: 'SQL_SERVER_PASSWORD'
     },    
     configPart2: {
         fName: 'Sonu',
